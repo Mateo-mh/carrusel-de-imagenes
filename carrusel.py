c246f84f5e900625b650f9a8ff2799960f560bb9
Mateo-mh <byronmh8@gmail.com>
2023-01-02 16:52:44 -0500

    Creamos 3 variables que contiene cada una su respectiva imagen

---
+++ carrusel.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from PIL import Image, ImageTk
+
+root = Tk()
+root.title('Carrusel')
+
+img1 = imageTk.photoimage(Image.open('images/01.jpg'))
+img1 = imageTk.photoimage(Image.open('images/02.jpg'))
+img1 = imageTk.photoimage(Image.open('images/03.jpg'))
+img1 = imageTk.photoimage(Image.open('images/04.jpg'))
